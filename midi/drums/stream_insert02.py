d6fbcef27191d721f4d09355994db21cc43bbe01
aadennis <dennis.circ@gmail.com>
2025-11-07 16:18:08 +0000

    er

--- midi/drums/stream_insert02.py
+++ midi/drums/stream_insert02.py
@@ -7,9 +7,9 @@ s1.insert(0.0, ts)
 s1.insert(0.0, mm)
 
 origin = 0.0
-s1.insert((0.0 + 0/3), note.Note(midi=36, quarterLength=1/3))
-s1.insert(0.0 + 1/3, note.Note(midi=38, quarterLength=1/3))
-s1.insert(0.0 + 2/3, note.Note(midi=42, quarterLength=1/3))
-s1.insert(0.0 + 3/3, note.Note(midi=46, quarterLength=1/3))
+s1.insert(origin + 0/3, note.Note(midi=36, quarterLength=1/3))
+s1.insert(origin + 1/3, note.Note(midi=38, quarterLength=1/3))
+s1.insert(origin + 2/3, note.Note(midi=42, quarterLength=1/3))
+s1.insert(origin + 3/3, note.Note(midi=46, quarterLength=1/3))
 
-s1.write('midi', fp="c:/temp/stream_insert01.mid")
+s1.write('midi', fp="c:/temp/stream_insert02.mid")
