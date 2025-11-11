87237aa78296fd09298d5b855a41a992050b43a7
aadennis <denniswells@gmail.com>
2025-11-11 14:25:37 +0000

    add to demo

--- midi/drums/music21/demo_for_md/read_config_01.py
+++ midi/drums/music21/demo_for_md/read_config_01.py
@@ -1,11 +1,14 @@
 from json import load
+from music21 import meter, tempo
 
 with open('music21/demo_for_md/read_config_01.json','r') as f:
     configData = load(f)
 
-bpm = configData.get("bpm")
-quarterLength = configData.get("quarterLength")
-ts = configData.get("TimeSignature")
+bpm_config = configData.get("bpm")
+quarterLength_config = configData.get("quarterLength")
+ts_config = configData.get("TimeSignature")
 
-
+ts = meter.TimeSignature(ts_config)
+mm = tempo.MetronomeMark(number=bpm_config)
+print(f"Time Signature: {ts_config}, BPM: {bpm_config}, quarterLength: {quarterLength_config}")
 
