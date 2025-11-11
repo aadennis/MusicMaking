9edcac71ae3fd634acf462a8847c991d02696874
aadennis <denniswells@gmail.com>
2025-11-11 19:41:51 +0000

    refactor

--- midi/drums/music21/demo_for_md/read_config_01.py
+++ midi/drums/music21/demo_for_md/read_config_01.py
@@ -2,12 +2,12 @@ from json import load
 from music21 import meter, tempo, stream, note
 from pandas import read_csv
 
-def insert_note(stream_obj, midi_note, velocity, quarterLength, index):
+def insert_note(stream_obj, midi_data_row, quarterLength, index):
     offset = quarterLength * index
     n = note.Note()
-    n.pitch.midi = midi_note
+    n.pitch.midi = midi_data_row['Note']
     n.quarterLength = quarterLength
-    n.volume.velocity = velocity
+    n.volume.velocity = midi_data_row['Velocity']
     stream_obj.insert(offset, n)
 
 drum_stream = stream.Stream()
@@ -26,12 +26,12 @@ drum_stream.insert(0.0, mm)
 
 midi_data_kick = read_csv('music21/demo_for_md/demo_kick.csv')
 for i, row in midi_data_kick.iterrows():
-    insert_note(drum_stream, row['Note'], row['Velocity'], quarterLength_config, i)
+    insert_note(drum_stream, row, quarterLength_config, i)
 
 # that was kick. Do it all again for the snare...
 midi_data_snare = read_csv('music21/demo_for_md/demo_snare.csv')
 for i, row in midi_data_snare.iterrows():
-    insert_note(drum_stream, row['Note'], row['Velocity'], quarterLength_config, i)
+    insert_note(drum_stream, row, quarterLength_config, i)
 
-drum_stream.write('midi', fp="c:/temp/demo_04.mid")
+drum_stream.write('midi', fp="c:/temp/demo_05.mid")
     
