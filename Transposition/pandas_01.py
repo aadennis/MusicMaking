2fd8e812eed0b3744aa2a6caf4bf9d054141f072
aadennis <dennis.circ@gmail.com>
2026-03-14 10:22:45 +0000

    unrelated build test

--- Transposition/pandas_01.py
+++ Transposition/pandas_01.py
@@ -3,7 +3,7 @@ import pandas as pd
 chord_file = 'transpose.csv'
 x = pd.read_csv(chord_file)
 
-sets = x.groupby('transposition') 
+sets = x.groupby('transposition')  
 
 filtered = x[x['transposition'] == 'C-D']
 print(filtered)
